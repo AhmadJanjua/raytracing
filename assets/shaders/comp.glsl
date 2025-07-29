1f68ecc170694a76784679410e7c57d71b44b554
Ahmad Janjua <ahmad.janjua@ucalgary.ca>
2025-07-29 12:18:45 -0600

    Added material to the spheres

--- assets/shaders/comp.glsl
+++ assets/shaders/comp.glsl
@@ -36,7 +36,7 @@ vec3 randUnitVec() {
         float distSquare = dot(point, point);
 
         if (0 < distSquare) {
-            return point / sqrt(distSquare);
+            return normalize(point);
         }
     }
 }
@@ -47,10 +47,35 @@ struct Ray {
     vec3 direction;
 };
 
+const int MAT_LAMBERTIAN = 0;
+const int MAT_METAL = 1;
+
+struct Material {
+    int type;
+    vec3 albedo;
+    float fuzz;
+};
+
+Material Lambertian(vec3 a) {
+    Material mat;
+    mat.type = MAT_LAMBERTIAN;
+    mat.albedo = a;
+    return mat;
+}
+
+Material Metal(vec3 a, float f) {
+    Material mat;
+    mat.type = MAT_METAL;
+    mat.albedo = a;
+    mat.fuzz = f;
+    return mat;
+}
+
 struct RayRecord {
     float time;
     vec3 point;
     vec3 normal;
+    Material material;
 };
 
 vec3 lookAt(Ray ray, float t) {
@@ -58,67 +83,81 @@ vec3 lookAt(Ray ray, float t) {
 }
 
 // ---- Shape Objects + constructors
-int SPHERE_TYPE = 0;
+const int SPHERE_TYPE = 0;
 
 struct Object {
     int type;
     vec3 center;
     float radius;
+    Material material;
 };
 
-Object Sphere(vec3 center, float radius) {
+Object Sphere(vec3 center, float radius, Material mat) {
     Object o;
     o.type = SPHERE_TYPE;
     o.center = center;
     o.radius = radius;
+    o.material = mat;
     return o;
 }
 
 // ---- World State
 Object world[] = {
-    Sphere(vec3(0, 0, -1), 0.5),
-    Sphere(vec3(0, -100.5, -1), 100)
+    Sphere(vec3( 0,      0, -1), 0.5, Lambertian(vec3(0.8, 0.3, 0.3))),
+    Sphere(vec3( 0, -100.5, -1), 100, Lambertian(vec3(0.8, 0.8, 0.0))),
+    Sphere(vec3( 1,      0, -1), 0.5, Metal(vec3(0.8, 0.6, 0.2), 0.9)),
+    Sphere(vec3(-1,      0, -1), 0.5, Metal(vec3(0.8, 0.8, 0.8), 0.1)),
 };
 
-// ---- Object Intersection
-vec3 defuseBounce(vec3 normal) {
-    vec3 unitVec = randUnitVec();
-
-    if (dot(unitVec, normal) > 0.0)
-        return unitVec;
-    else
-        return -unitVec;
+// Scattering properties
+vec3 reflect(vec3 v, vec3 norm) {
+    return v - 2 * dot(v, norm) * norm;
 }
 
-bool intersectSphere(Object o, Ray r, inout RayRecord rec, float tMin, float tMax) {
-    vec3 oc = r.origin - o.center;
-
-    float a = dot(r.direction, r.direction);
-    float b = dot(oc, r.direction);
-    float c = dot(oc, oc) - o.radius*o.radius;
-    float discriminant = b*b - a*c;
-
-    if (discriminant > 0) {
-        float t = (-b - sqrt(discriminant)) / a;
-
-        if (t < tMin || t > tMax) {
-            t = (-b + sqrt(discriminant)) / a;
-        }
-
-        if (t > tMin && t < tMax) {
-            rec.time = t;
-            rec.point = lookAt(r, t);
-            rec.normal = (rec.point - o.center) / o.radius;
+bool scatter(Ray inRay, inout Ray scatterRay, inout RayRecord rec, inout vec3 attenuation) {
+    switch (rec.material.type) {
+        case MAT_LAMBERTIAN:
+            vec3 direction = rec.normal + randUnitVec();
+            scatterRay = Ray(rec.point, direction);
+            attenuation *= rec.material.albedo;
             return true;
-        }
+        case MAT_METAL:
+            vec3 reflected = reflect(normalize(inRay.direction), rec.normal);
+            scatterRay = Ray(rec.point, reflected + rec.material.fuzz * randUnitVec());
+            attenuation *= rec.material.albedo;
+            return (dot(scatterRay.direction, rec.normal) > 0);
     }
-
     return false;
 }
 
+// ---- Object Intersection
 bool intersect(Object o, Ray r, inout RayRecord rec, float tMin, float tMax) {
     switch (o.type) {
-        case 0: return intersectSphere(o, r, rec, tMin, tMax);
+        case SPHERE_TYPE:
+            vec3 oc = r.origin - o.center;
+
+            float a = dot(r.direction, r.direction);
+            float b = dot(oc, r.direction);
+            float c = dot(oc, oc) - o.radius*o.radius;
+            float discriminant = b*b - a*c;
+
+            if (discriminant > 0) {
+                float t = (-b - sqrt(discriminant)) / a;
+
+                if (t < tMin || t > tMax) {
+                    t = (-b + sqrt(discriminant)) / a;
+                }
+
+                if (t > tMin && t < tMax) {
+                    rec.time = t;
+                    rec.point = lookAt(r, t);
+                    rec.normal = (rec.point - o.center) / o.radius;
+                    rec.material = o.material;
+                    return true;
+                }
+            }
+
+            return false;
     }
     return false;
 }
@@ -139,16 +178,16 @@ bool findIntersect(Ray r, inout RayRecord rec, float tMin, float tMax) {
 
 // ---- Get the color
 vec3 color(Ray ray) {
-    float attenuation = 1.0;
+    vec3 attenuation = vec3(1.0);
     const int maxBounces = 10;
 
     for (int i = 0; i < maxBounces; ++i) {
         RayRecord rec;
 
         if (findIntersect(ray, rec, 0.001, 3.402823466e+38)) {
-            vec3 direction = rec.normal + defuseBounce(rec.normal);
-            ray = Ray(rec.point, direction);
-            attenuation *= 0.5;
+            if (!scatter(ray, ray, rec, attenuation)) {
+                break;
+            }
         } else {
             vec3 dir_norm = normalize(ray.direction);
             float t = 0.5 * (dir_norm.y + 1.0);
@@ -158,7 +197,6 @@ vec3 color(Ray ray) {
 
     return vec3(0, 0, 0);
 }
-
 
 // Cast ray and anti-alias the color results
 void main() {
